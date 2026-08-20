66cdb58391051567f1d5f485b09628adb9c100d9
mghrbymslm705-lang <mghrbymslm705-lang@users.noreply.github.com>
2026-08-20 19:09:28 +0200

    Update: اضافة شاشة جانبية (دراور)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "696986472" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2835460137" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
