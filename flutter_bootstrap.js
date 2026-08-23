65698689e78ea970dfca3d1a63f04f8227a65aa4
mghrbymslm705-lang <mghrbymslm705-lang@users.noreply.github.com>
2026-08-24 00:13:18 +0200

    Deploy: fix install button to trigger PWA prompt

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1138491643" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1003133397" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
